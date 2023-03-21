198c5da07a67cea20f2ecd99a5ed479e86dc5b19
Hannes Achleitner <hannes.software@gmx.at>
2023-03-21 11:08:03 +0100

    List GPU devices

--- playground/testInstallation.py
+++ playground/testInstallation.py
@@ -5,7 +5,8 @@ print("")
 print("OpenCV     version: {}".format(cv2.__version__))
 print("Keras      version: {}".format(tf.keras.__version__))
 print("Tensorflow version: {}".format(tf.__version__))
-print("Num GPUs available: {}".format(len(tf.config.list_physical_devices('GPU'))))
+print("Num Physical  GPUs: {}".format(len(tf.config.list_physical_devices('GPU'))))
+print("Num Logical   GPUs: {}".format(len(tf.config.list_logical_devices('GPU'))))
 
 # https://www.tensorflow.org/guide/gpu#logging_device_placement
 tf.debugging.set_log_device_placement(True)
